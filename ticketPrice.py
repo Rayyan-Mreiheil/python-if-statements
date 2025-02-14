962eb9faa7147a2b0510197cf0d2934ffaefe5ed
Rayyan-Mreiheil <rayyanmwork@gmail.com>
2025-02-14 22:42:07 +0200

    Initialize <> evenOddnb.py

--- ticketPrice.py
+++ ticketPrice.py
@@ -3,3 +3,9 @@ age = int(input("Enter your age: "))
 
 if age <= 10:
     print("The ticket price is $5.")
+elif age <= 20:
+    print("The ticket price is $10.")
+elif age <= 30:
+    print("The ticket price is $15.")
+else:
+    print("The ticket price is $20.")
